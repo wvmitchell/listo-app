409e4dc3140f81308148912a4e12b71ac3a1509e
Will Mitchell <wvmitchell@gmail.com>
2024-07-19 22:12:38 -0400

    add todo

--- app/api/checklists/route.ts
+++ app/api/checklists/route.ts
@@ -1,6 +1,7 @@
 import { getChecklists } from "@/api/checklistAPI"
 import { NextResponse } from "next/server"
 
+// TODO: add wrapped routes with authentication
 export async function GET() {
   const response = await getChecklists()
   return NextResponse.json(response)
