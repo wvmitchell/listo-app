ca24037345bb1a876491c3806b1ddefd841fdc5d
Will Mitchell <wvmitchell@gmail.com>
2024-07-19 23:08:24 -0400

    bugfix: queryProvider has to be set outsite of component

--- app/(checklists)/layout.tsx
+++ app/(checklists)/layout.tsx
@@ -5,9 +5,9 @@ import { useRouter } from "next/navigation"
 import { useUser } from "@auth0/nextjs-auth0/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Header from "@/app/components/Header"
+const queryClient = new QueryClient()
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient()
   const { user, isLoading } = useUser()
   const router = useRouter()
 
