ec10e51ab98328f0d013b5d6b039d6abbf7f219e
Will Mitchell <wvmitchell@gmail.com>
2024-07-21 12:38:22 -0400

    remove unused route

--- app/api/checklists/route.ts
+++
@@ -1,8 +0,0 @@
-import { getChecklists } from "@/api/checklistAPI"
-import { NextResponse } from "next/server"
-
-// TODO: add wrapped routes with authentication
-export async function GET() {
-  const response = await getChecklists()
-  return NextResponse.json(response)
-}
