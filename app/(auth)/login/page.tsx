6a6d1ef41ea8aae2834f862642421e3dbf870fac
Will Mitchell <wvmitchell@gmail.com>
2024-07-22 17:04:42 -0400

    text change

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -24,7 +24,7 @@ const LoginPage = async () => {
           href="/api/auth/login"
           className="mt-5 rounded-md border border-slate-500 bg-white px-3 py-2 text-sm font-semibold text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 hover:bg-slate-50"
         >
-          Get Started
+          Get Started!
         </a>
       </div>
     )
