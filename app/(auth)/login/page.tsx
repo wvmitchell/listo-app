0988a911440d974357207aaf01b666d98bde02ff
Will Mitchell <wvmitchell@gmail.com>
2024-07-22 16:49:14 -0400

    remove wrapping button

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -20,9 +20,12 @@ const LoginPage = async () => {
           height={200}
           alt="Listo. Get it done together."
         />
-        <button className="mt-5 rounded-md border border-slate-500 bg-white px-3 py-2 text-sm font-semibold text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 hover:bg-slate-50">
-          <a href="/api/auth/login">Get Started</a>
-        </button>
+        <a
+          href="/api/auth/login"
+          className="mt-5 rounded-md border border-slate-500 bg-white px-3 py-2 text-sm font-semibold text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 hover:bg-slate-50"
+        >
+          Get Started
+        </a>
       </div>
     )
   )
