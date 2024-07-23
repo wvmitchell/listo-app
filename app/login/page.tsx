be65645c3c539a49bb14fbd17964434494ccfe6c
Will Mitchell <wvmitchell@gmail.com>
2024-07-23 17:58:01 -0400

    move import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,8 @@
 import { getSession } from "@auth0/nextjs-auth0"
 import { redirect } from "next/navigation"
 import Image from "next/image"
-import { userExists, createUserProfile } from "@/utils/userAPI"
 import logoWithTagline from "@/app/images/full_logo_with_tagline.png"
+import { userExists, createUserProfile } from "@/utils/userAPI"
 import Auth0Link from "./auth0Link"
 
 const LoginPage = async () => {
