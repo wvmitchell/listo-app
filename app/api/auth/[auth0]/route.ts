ca0163f824958766dc0f490863d1924ac4c95671
Will Mitchell <wvmitchell@gmail.com>
2024-07-23 09:41:29 -0400

    try something to see if it shows up on the server

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,17 +1,21 @@
 import { NextResponse } from "next/server"
-import { handleAuth } from "@auth0/nextjs-auth0"
+//import { handleAuth } from "@auth0/nextjs-auth0"
 
 //export const GET = handleAuth()
 
-export async function GET(req: Request, res: Response) {
-  console.log("GET request: ", req)
-  try {
-    return handleAuth()(req, res)
-  } catch (error: any) {
-    console.error(error)
-    return NextResponse.json(
-      { error: "Could not handle request" },
-      { status: 500 },
-    )
-  }
+//export async function GET(req: Request, res: Response) {
+//  console.log("GET request: ", req)
+//  try {
+//    return handleAuth()(req, res)
+//  } catch (error: any) {
+//    console.error(error)
+//    return NextResponse.json(
+//      { error: "Could not handle request" },
+//      { status: 500 },
+//    )
+//  }
+//}
+
+export async function GET(req: Request) {
+  return NextResponse.json({ body: req.url })
 }
