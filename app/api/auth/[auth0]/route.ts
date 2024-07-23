914b95b0a929da5c12183d65213da31e53bdd66b
Will Mitchell <wvmitchell@gmail.com>
2024-07-23 09:32:11 -0400

    add logging to to dynamic route

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,7 +1,17 @@
+import { NextResponse } from "next/server"
 import { handleAuth } from "@auth0/nextjs-auth0"
 
-export const GET = handleAuth({
-  onError: (error: any) => {
-    console.error("Auth routing error: ", error)
-  },
-})
+//export const GET = handleAuth()
+
+export async function GET(req: Request, res: Response) {
+  console.log("GET request: ", req)
+  try {
+    return handleAuth()(req, res)
+  } catch (error: any) {
+    console.error(error)
+    return NextResponse.json(
+      { error: "Could not handle request" },
+      { status: 500 },
+    )
+  }
+}
