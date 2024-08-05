cfd6b325db15405a17a5dd65b56957e63369a2d1
Will Mitchell <wvmitchell@gmail.com>
2024-08-05 10:30:02 -0400

    remove unused import

--- app/(componentRoutes)/(checklists)/[id]/components/NewItemForm.tsx
+++ app/(componentRoutes)/(checklists)/[id]/components/NewItemForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useRef } from "react"
 import { PlusCircleIcon } from "@heroicons/react/24/outline"
 import { checkForEnter } from "@/utils/domUtils"
 
